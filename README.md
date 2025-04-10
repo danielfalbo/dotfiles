c832c7d6a5c1e4ac7cdb33682736650bebab772e
Daniel <39460524+danielfalbo@users.noreply.github.com>
2025-04-10 02:05:16 +0200

    [install.sh] --no-gitconfig option

--- README.md
+++ README.md
@@ -7,5 +7,5 @@
 macOS
 
 ```bash
-git clone https://github.com/danielfalbo/dotfiles ~/.dotfiles && ~/.dotfiles/install.sh
+git clone https://github.com/danielfalbo/dotfiles ~/.dotfiles && ~/.dotfiles/install.sh --no-gitconfig
 ```
--- install.sh
+++ install.sh
@@ -16,14 +16,16 @@ mkdir -p ~/.config/karabiner
 mkdir -p ~/Library/Application\ Support/Cursor/User
 
 /bin/rm -f ~/.config/karabiner/karabiner.json
-/bin/rm -f ~/.gitconfig
 /bin/rm -f ~/.vimrc
 /bin/rm -f ~/.zshrc
 /bin/rm -f ~/Library/Application\ Support/Code/User/settings.json
 
-# create symlinks
+if [[ "$1" != "--no-gitconfig" ]]; then
+    /bin/rm -f ~/.gitconfig
+    ln -sf "${BASEDIR}/.gitconfig" ~/.gitconfig
+fi
+
 ln -sf "${BASEDIR}/karabiner.json" ~/.config/karabiner/karabiner.json
-# ln -sf "${BASEDIR}/.gitconfig" ~/.gitconfig
 ln -sf "${BASEDIR}/.vimrc" ~/.vimrc
 ln -sf "${BASEDIR}/.zshrc" ~/.zshrc
 ln -sf "${BASEDIR}/settings.json" ~/Library/Application\ Support/Code/User/settings.json
